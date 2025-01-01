a57f70e7b7f5b2f364cb7a624ea1246128cfb3c0
Manish_Syal <manishsyal77@gmail.com>
2025-01-01 17:57:29 +0530

    Not found Page added with Lottie Animation.

---
+++ src/app/not-found.js
@@ -0,0 +1,37 @@
+"use client";
+import Lottie from "lottie-react";
+import Link from "next/link";
+import notfound from "../../public/notfound.json";
+import { Button } from "@/components/ui/button";
+import { ArrowLeftCircle } from "lucide-react";
+
+export default function NotFound() {
+  return (
+    <div className="grid h-screen place-content-center bg-white px-4">
+      <div className="flex flex-col items-center text-center">
+        {/* <h1 className="text-9xl font-black text-gray-200">404</h1> */}
+        <Lottie
+          animationData={notfound}
+          loop={true}
+          width="800"
+          height="800"
+          className="items-center justify-center"
+        />
+        <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Uh-oh!
+        </p>
+
+        <p className="mt-4 text-gray-500">We can't find that page.</p>
+
+        <Link
+          href="/dashboard"
+          className="mt-6 inline-block rounded bg-indigo-600 px-5 py-3 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring"
+        >
+          <Button>
+            <ArrowLeftCircle className="mr-1" /> Go Home
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+}
